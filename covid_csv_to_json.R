ab95925cb053a7b5befa58cfdc9a950d6b9b559d
goeranh <goeranh@users.noreply.github.com>
2021-05-05 15:27:14 +0200

    update 210505

--- covid_csv_to_json.R
+++ covid_csv_to_json.R
@@ -13,9 +13,8 @@ library(jsonlite)
 df <- read_excel("data/vgr_data.xlsx",
                  skip = 2)
 wf <- df %>% group_by(Datum) %>%
-  summarise(sjh_tot = sum(Inneliggande),
-            iva = sum(`varav IVA`)) %>%
-  filter(Datum < "2021-01-01")
+  summarise(sjh_tot = sum(Inneliggande, na.rm = TRUE),
+            iva = sum(`varav IVA`, na.rm = TRUE))
 
 ggplot(wf, aes(x = Datum))+
   geom_line(aes(y = sjh_tot))+
@@ -23,3 +22,12 @@ ggplot(wf, aes(x = Datum))+
 
 write_csv(wf, "data/covid.csv")
 
+wf_js <- toJSON(wf)
+
+wf_mx <- as.matrix(wf)
+toJSON(wf_mx)
+coln <- colnames(wf_mx)
+wf2 <- rbind(coln, wf_mx) %>% as_tibble()
+wf2_js <- toJSON(wf2, dataframe = "values")
+wf2_js
+write(wf2_js, "covid_json.json")
