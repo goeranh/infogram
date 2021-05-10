8984b409006d8d1daf017a6873fef0670304f200
goeranh <goeranh@users.noreply.github.com>
2021-05-10 09:52:36 +0200

    test med matrix

--- covid_csv_to_json.R
+++ covid_csv_to_json.R
@@ -31,3 +31,50 @@ wf2 <- rbind(coln, wf_mx) %>% as_tibble()
 wf2_js <- toJSON(wf2, dataframe = "values")
 wf2_js
 write(wf2_js, "covid_json.json")
+
+
+
+# Uppdateriing 210505 -----------------------------------------------------
+
+df2 <- read_excel("data/vgr_data_2.xlsx",
+                  sheet = 3,
+                 skip = 3)
+wf2 <- df2 %>% group_by(Datum) %>%
+  summarise(sjh_tot = sum(Inneliggande, na.rm = TRUE),
+            iva = sum(`varav IVA`, na.rm = TRUE))
+
+ggplot(wf, aes(x = Datum))+
+  geom_line(aes(y = sjh_tot))+
+  geom_line(aes(y = iva), color = 2)
+
+wf_mx2 <- as.matrix(wf2)
+toJSON(wf_mx2)
+coln <- colnames(wf_mx2)
+wf2 <- rbind(coln, wf_mx2) %>% as_tibble()
+wf2_js <- toJSON(wf2, dataframe = "values")
+wf2_js
+write(wf2_js, "covid_json.json")
+
+
+# Uppdatering 210506 ------------------------------------------------------
+
+df2 <- read_excel("data/vgr_data_2.xlsx",
+                  sheet = 3,
+                  skip = 3)
+wf2 <- df2 %>% group_by(Datum) %>%
+  summarise(sjh_tot = sum(Inneliggande, na.rm = TRUE),
+            iva = sum(`varav IVA`, na.rm = TRUE))
+
+ggplot(wf, aes(x = Datum))+
+  geom_line(aes(y = sjh_tot))+
+  geom_line(aes(y = iva), color = 2)
+
+wf_mx2 <- as.matrix(wf2)
+toJSON(wf_mx2)
+coln <- colnames(wf_mx2)
+wf2 <- rbind(coln, wf_mx2) %>% as_tibble()
+wf2_js <- toJSON(wf2, dataframe = "values")
+wf2_js
+write(wf2_js, "covid_json.json")
+
+
